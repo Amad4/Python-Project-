53f2816dbeb93d2a2c05bf5e9125c888d94973a3
Amad4 <98763026+Amad4@users.noreply.github.com>
2022-05-01 15:46:12 -0400

    Add files via upload

---
+++ Python Project data.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+
+df = pd.read_csv(r'Olist_customers_dataset.csv')
+df.head(21)
+
+import pandas as pd
+df = pd.read_csv(r'Olist_geolocation_dataset.csv')
+df.head(21)
+
+import pandas as pd
+df = pd.read_csv(r'Olist_order_items.csv')
+df.head(21)
+
+import pandas as pd
+df = pd.read_csv(r'Olist_order_payments.csv')
+df.head(21)
+
+import pandas as pd
+df = pd.read_csv(r'Olist_order_reviews.csv')
+df.head(21)
+
+import pandas as pd
+df = pd.read_csv(r'Olist_products.csv')
+df.head(21)
+
+import pandas as pd
+df = pd.read_csv(r'Olist_sellers.csv')
+df.head(100)
+
